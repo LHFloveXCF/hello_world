705c9c7bf27ef519736ef4a89029afdcf0315864
xuchaofeng <826639991@qq.com>
2021-03-09 10:43:39 +0800

    第一个修改java文件！

--- src/main/java/com/xu/calculator/TestGit.java
+++ src/main/java/com/xu/calculator/TestGit.java
@@ -6,5 +6,6 @@ package com.xu.calculator;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git程序，请大佬多多指教！");
+        System.out.println("终于成功了！");
     }
 }
