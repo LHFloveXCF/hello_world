23250cfa97f05b4b3a19a33b9bb0525e89cea400
xuchaofeng <826639991@qq.com>
2021-03-09 10:28:23 +0800

    第一个java程序，给大佬们问好！！！

---
+++ src/main/java/com/xu/calculator/TestGit.java
@@ -0,0 +1,10 @@
+package com.xu.calculator;
+
+/**
+ * hello_world
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("这是我的第一个git程序，请大佬多多指教！");
+    }
+}
